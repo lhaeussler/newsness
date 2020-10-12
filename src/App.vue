bbc6e125361e73cd7f9651b57ea953d17c5bc5b0
lhaeussler <71760301+lhaeussler@users.noreply.github.com>
2020-10-12 18:51:58 +0200

    Menu showoff

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <menu></menu>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Menu from "./components/menu";
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Menu
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,13 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import router from './router'
 
 Vue.config.productionTip = false
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  render: h => h(App),
   router,
   components: { App },
   template: '<App/>'
