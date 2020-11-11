848a0686cad4544b9251f5c6e01a29f2d131e6b3
raef722 <raef722@gmail.com>
2020-11-11 10:04:27 +0100

    password bearbeitung

--- web_frontend/src/components/register.vue
+++ web_frontend/src/components/register.vue
@@ -15,10 +15,10 @@
       <b-form-input id="example-input" label="Birthdate" v-model="value" type="date" autocomplete="off"></b-form-input>
     </b-input-group>
   <b-form-group id="input-vorname" label="Password" label-for="input-2">
-        <b-form-input id="input-2" v-model="form.vorname" required></b-form-input>
+        <b-form-input id="input-2" v-model="form.password" type="password" required></b-form-input>
       </b-form-group>
       <b-form-group id="input-Surname" label="Repeat Password" label-for="input-3">
-        <b-form-input id="input-3" v-model="form.nachname" type="password" required></b-form-input>
+        <b-form-input id="input-3" v-model="form.repeatpassword" type="password" required></b-form-input>
       </b-form-group>
       <b-button type="submit" variant="primary" size="lg">Register</b-button>
     </b-form>
@@ -36,7 +36,9 @@
           nachname:'',
           selected: '',
           value:'',
-          formatted: ''
+          formatted: '',
+          password:'',
+          repeatpassword:'',
 
 
         },
